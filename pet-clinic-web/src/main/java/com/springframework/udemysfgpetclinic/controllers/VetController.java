175f1318cf373f9879630b9a1b14c67d72a4a8ab
kheesiang <kheesiang98@gmail.com>
2021-06-06 23:13:54 +0800

    Add vets index page and controller

---
+++ pet-clinic-web/src/main/java/com/springframework/udemysfgpetclinic/controllers/VetController.java
@@ -0,0 +1,18 @@
+package com.springframework.udemysfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Khee Siang
+ */
+@Controller
+public class VetController {
+
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    public String listVets() {
+
+        return "vets/index";
+
+    }
+}
