f1844cdb4d30fa8796318dea3659ee452cf25827
kheesiang <kheesiang98@gmail.com>
2021-06-07 23:58:53 +0800

    Refactor to auto generate map ids

--- pet-clinic-data/src/main/java/com/springframework/udemysfgpetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/com/springframework/udemysfgpetclinic/services/map/AbstractMapService.java
@@ -1,16 +1,15 @@
 package com.springframework.udemysfgpetclinic.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import com.springframework.udemysfgpetclinic.model.BaseEntity;
+
+import java.util.*;
 
 /**
  * @author Khee Siang
  */
-public abstract class AbstractMapService<T, ID> {
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
 
-    protected Map<ID, T> map = new HashMap<>();
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll() {
         return new HashSet<>(map.values());
@@ -20,8 +19,17 @@ public abstract class AbstractMapService<T, ID> {
         return map.get(id);
     }
 
-    T save(ID id, T object) {
-        map.put(id, object);
+    T save(T object) {
+
+        if(object != null) {
+            if(object.getId() == null) {
+                object.setId(getNextId());
+            }
+
+            map.put(object.getId(), object);
+        } else {
+            throw new RuntimeException("Object cannot be null");
+        }
 
         return object;
     }
@@ -32,5 +40,18 @@ public abstract class AbstractMapService<T, ID> {
 
     void delete(T object) {
         map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+    }
+
+    private Long getNextId() {
+
+        Long nextId = null;
+
+        try {
+            nextId = Collections.max(map.keySet()) + 1;
+        } catch (NoSuchElementException e) {
+            nextId = 1L;
+        }
+
+        return nextId;
     }
 }
